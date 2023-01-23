2d7d2225be85d8789e699b727a8ed55f53a16749
Ricardo-Suarez-Chacon <Ricardosua@gmail.com>
2023-01-22 22:29:24 -0500

    step 3 All inputs and Buton Calculate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import styled from 'styled-components';
 import {Formik, Form } from 'formik';
 import Input from './components/Input';
+import Button from './components/Button';
+import Container from './components/Container';
+import Section from './components/Section';
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  height: 100%;
-  align-items: center;
-`
 
-const Section = styled.section`
-  background-color: #eee;
-  border-top: solid 2px palevioletred;
-  padding: 20px 25px;
-  width: 500px;
-  box-shadow: 0px 2px 3 px rgb(0,0,0,0.3);
-`
+
+
 
 function App() {
   const handleSubmit = ()=>{}
@@ -33,6 +24,10 @@ function App() {
         >
           <Form>
             <Input name="deposit" label="Initial Deposit" />
+            <Input name="contribution" label="Annual contribution" />
+            <Input name="years" label="Years" />
+            <Input name="rate" label="Interest rate" />
+            <Button>Calculate</Button>
           </Form>
         </Formik>
       </Section>
